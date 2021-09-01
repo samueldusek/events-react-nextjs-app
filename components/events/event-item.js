6e39e9decd0f16dba38c139d136b82f222b60fd1
samueldusek <smldsk@protonmail.com>
2021-09-01 13:44:22 +0200

    add Image component to all images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -2,6 +2,7 @@ import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
+import Image from "next/image";
 
 import classes from "./event-item.module.css";
 
@@ -20,7 +21,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
