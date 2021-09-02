cc23f7b92d30e51f4cf74a1e322d10a4c3ee3e7f
samueldusek <smldsk@protonmail.com>
2021-09-02 11:10:45 +0200

    add option to save and fetch comments to and from database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,10 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   const { eventId } = req.query;
+
+  const client = await MongoClient.connect(process.env.DB_URL);
+  const db = client.db();
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -14,21 +19,32 @@ function handler(req, res) {
       return;
     }
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
+
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
+
+    newComment.id = result.insertedId;
+
     res.status(201).json({ message: "Added comment.", comment: newComment });
     console.log(newComment);
   }
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Sam", text: "Cool Stuff" },
-      { id: "c2", name: "Max", text: "Great event" },
-    ];
-    res.status(200).json({ comments: dummyList });
+    const comments = await db
+      .collection("comments")
+      .find({ eventId })
+      .sort({ _id: -1 })
+      .toArray();
+
+    res.status(200).json({ comments: comments });
   }
+
+  client.close();
 }
 
 export default handler;
